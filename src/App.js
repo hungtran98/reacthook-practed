ec8a2f38aa8c6e82e1213f47f6ab6adf4b3f34d2
hungtran98 <thanhhunghg98@gmail.com>
2023-06-25 19:51:38 +0700

    route and design Login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.scss';
 import User from '../src/pages/User/User'
 import Home from '../src/pages/Home/Home'
+import Login from '../src/pages/Login/Login'
 import { Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
    <Routes>
     <Route path='/' element={<Home />}/>
     <Route path='/users' element={<User />}/>
+    <Route path='/login' element={<Login />}/>
    </Routes>
   );
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './login.scss'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [isShowPassword, setIsShowPassword] = useState(false)
+
   return (
-    <div>Login</div>
+    <div className='login-container col-12 col-sm-4'>
+      <h2>Log in</h2>
+      <div className='text'>Email or username</div>
+      <div className='inputs'>
+        <input className='input' placeholder='Email or username' value={email} onChange={(e) => setEmail(e.target.value) }/>
+      </div>
+      <div className='inputs'>
+        <input className='input' type={isShowPassword ? 'text' : 'password'} placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value) }/>
+        <div className='eye'><i onClick={() => setIsShowPassword(!isShowPassword)} className={isShowPassword ? "fa-solid fa-eye" : "fa-solid fa-eye-slash" }></i></div>
+      </div>
+      <button className={email && password ? 'active' : 'button-login'} >Log in</button>
+      <div className='button-back'><i className="fa-solid fa-chevron-left"></i>Go back</div>
+    </div>
   )
 }
 
